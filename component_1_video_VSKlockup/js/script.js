97be5921acb022f051840bf4f2bad5f37431a6ef
mirekwcz <mirekwcz@gmail.com>
2017-11-19 23:18:50 +1100

    almost 1st page is done

--- component_1_video_VSKlockup/js/script.js
+++ component_1_video_VSKlockup/js/script.js
@@ -1,18 +1,21 @@
 var globals = {}; 
 
 /*window.addEventListener("load", Init); */
-window.onload = function () {Init(body_wrapper_event_handler, animated_text_event_handler, animated_img_2_event_handler);}
-
-function Init(bw_ev_handler, at_ev_handler, ai2_ev_handler) {
+//window.onload = function () {Init(body_wrapper_ev_handler, animated_txt_ev_handler, animated_txt_2_ev_handler, animated_img_2_ev_handler);}
+window.onload = function () {Init ();};
+//function Init(bw_ev_handler, at_ev_handler, ai2_ev_handler) {
+function Init() {
     globals.body_wrapper = document.getElementById("body-wrapper");
     globals.animated_txt = document.getElementById("animated-txt");
+    globals.animated_txt_2 = document.getElementById("animated-txt-2");
     globals.animated_img_2 = document.getElementById("animated-img-2");
     globals.animated_video = document.getElementById("animated-video");
     pfx = ["webkit", "moz", "MS", "o", ""];
     // animation listener events
-    prefixed_event(globals.body_wrapper, "AnimationEnd", bw_ev_handler);
-    prefixed_event(globals.animated_txt, "AnimationEnd", at_ev_handler);
-    prefixed_event(globals.animated_img_2, "AnimationEnd", ai2_ev_handler);
+    prefixed_event(globals.body_wrapper, "AnimationEnd", body_wrapper_ev_handler);
+    prefixed_event(globals.animated_txt, "AnimationEnd", anim_txt_ev_handler);
+    prefixed_event(globals.animated_txt_2, "AnimationEnd", anim_txt_2_ev_handler);
+    prefixed_event(globals.animated_img_2, "AnimationEnd", anim_img_2_ev_handler);
 }
 
 //apply prefixed event handlers
@@ -24,15 +27,20 @@ function prefixed_event(element, type, callback) {
 }
 
 //--- event handlers / callback functions (one for each html element: 'body-wrapper', 'animated-txt' and 'animated-img-2' 
-function body_wrapper_event_handler(e) {
-    display_txt(); //display text: See the World Through Her Eyes
+function body_wrapper_ev_handler(e) {
+    display_txt(); //display text: See the World Through Her Eyes.
 }
 
-function animated_text_event_handler (e) {
+function anim_txt_ev_handler (e) {
     display_img_2 ();  // display an image of VSK (vegan starter kit) with text: then change it 
+    display_txt_2 ();  // display 2nd text: Then change it.
 }
 
-function animated_img_2_event_handler (e) {
+function anim_txt_2_ev_handler (e) {
+
+}
+
+function anim_img_2_ev_handler (e) {
     // display_video ();
 }
 
@@ -40,6 +48,10 @@ function animated_img_2_event_handler (e) {
 function display_txt() {
     globals.animated_txt.classList.add("fade-in-txt"); //execute fade-in text animation
 };
+
+function display_txt_2 () {
+    globals.animated_txt_2.classList.add("fade-in-txt"); //execute fade-in text animation
+}
 
 function display_img_2() {
     globals.animated_img_2.style.visibility="visible";
